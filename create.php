317540a8c5b59a42bd856e5c05e6bd668c737965
kapilrohilla225 <kapilrohilla206@gmail.com>
2024-04-13 17:18:53 +0530

    Design 2 pages create and index

---
+++ create.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD-with-PHP-OOP</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-tittle">CRUD-with-PHP-OOP MySQLi</h3>
+                    </div>
+                    <form>
+  <fieldset disabled>
+    <legend>Disabled fieldset example</legend>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Name</label>
+      <input type="text" id="name" class="form-control" placeholder="Enter Your Name">
+    </div>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Roll</label>
+      <input type="text" id="roll" class="form-control" placeholder="Enter Your Roll">
+    </div>
+    <div class="mb-3">
+      <label for="disabledTextInput" class="form-label">Course</label>
+      <input type="text" id="course" class="form-control" placeholder="Enter Your Course">
+    </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <a href="index.php" class="btn btn-primary">Go Back</a>
+  </fieldset>
+</form>
+                    <div class="card-footer">
+                        <h3 class="card-tittle">Developed By Geeta Technical Hub</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    include('header.php');
+?>
+                    <div class="card-body">
+                            <table class="table table-striped table-bordered table-hover">
+                                <tr>
+                                    <th>Serial</th>
+                                    <th>Name</th>
+                                    <th>Roll</th>
+                                    <th>Course</th>
+                                </tr>
+                                <tr>
+                                    <td>01</td>
+                                    <td>Sonu Kumar</td>
+                                    <td>123456789</td>
+                                    <td>B.TECH</td>
+                                </tr>
+                                <tr>
+                                    <td>02</td>
+                                    <td>Sonu Kumar</td>
+                                    <td>123456789</td>
+                                    <td>B.TECH</td>
+                                </tr>
+                                <tr>
+                                    <td>03</td>
+                                    <td>Sonu Kumar</td>
+                                    <td>123456789</td>
+                                    <td>B.TECH</td>
+                                </tr>
+                                <tr>
+                                    <td>04</td>
+                                    <td>Sonu Kumar</td>
+                                    <td>123456789</td>
+                                    <td>B.TECH</td>
+                                </tr>
+                                <tr>
+                                    <td>05</td>
+                                    <td>Sonu Kumar</td>
+                                    <td>123456789</td>
+                                    <td>B.TECH</td>
+                                </tr>
+                            </table>
+                            <a class="btn btn-success" href="create.php">Create</a>
+                    </div>
+<?php
+    include('footer.php');
+?>                    
